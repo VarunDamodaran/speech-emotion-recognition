140d74818041c0276f3ba61622a6e7c6db1025d8
VarunDamodaran <varundamodaran111@gmail.com>
2026-04-16 01:02:01 +0530

    path fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -114,7 +114,8 @@ export default function App() {
   const isRecordingRef = useRef(false)
 
   useEffect(() => {
-    const ws = new WebSocket("ws://127.0.0.1:8000/ws/stream")
+    const WS_URL = import.meta.env.VITE_WS_URL || "ws://127.0.0.1:8000/ws/stream";
+    const ws = new WebSocket(WS_URL);
     
     ws.onopen = () => {
       console.log("Connected to ML Server")
